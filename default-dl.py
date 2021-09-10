a33f5c5516e0002ed4740b3336a8367562b24580
Vincent <huangvincent170@gmail.com>
2021-09-10 19:38:21 -0400

    clean up code

--- default-dl.py
+++ default-dl.py
@@ -11,33 +11,21 @@ from eyed3.id3.frames import ImageFrame
 from PIL import Image
 import cv2
 
-if len(sys.argv) <= 1:
-    print("Please input a youtube URL.")
-    quit()
+VIDEO_ID_REGEX = re.compile(r"https:\/\/www\.youtube\.com\/watch\?v=([A-Za-z0-9\-_]+).*")
+DIR_PATH = "./Downloads"
+TEMP_PATH = f"{DIR_PATH}/temp"
 
-vid_urls = sys.argv[1:]
-
-reg = re.compile(r"https:\/\/www\.youtube\.com\/watch\?v=([A-Za-z0-9\-]+).*")
-vid_ids = [reg.match(vid_url).groups()[0] for vid_url in vid_urls]
-
-dirpath = "./Downloads"
-if os.path.exists(dirpath):
-    shutil.rmtree(dirpath)
-os.mkdir(dirpath)
-
-class MyLogger(object):
+class DlLogger(object):
     def debug(self, msg):
         pass
-
     def warning(self, msg):
         pass
-
     def error(self, msg):
         print(msg)
 
 start_dl = True
 dl_format = ""
-def my_hook(d):
+def dl_hook(d):
     global start_dl, dl_format
     if d['status'] == 'downloading':
         if not start_dl:
@@ -50,43 +38,63 @@ def my_hook(d):
         print(f"Processing {dl_format}...")
         start_dl = True
 
-dl_format = "Audio"
-ydl_opts = {
+AUDIO_YTDL_OPTS = {
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    'outtmpl': './Downloads/temp-%(id)s.%(ext)s',
-    'logger': MyLogger(),
-    'progress_hooks': [my_hook],
+    'outtmpl': f'{TEMP_PATH}/%(id)s.%(ext)s',
+    'logger': DlLogger(),
+    'progress_hooks': [dl_hook],
 }
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(vid_urls)
 
-dl_format = "Video"
-ydl_opts = {
+VIDEO_YTDL_OPTS = {
     'format': 'bestvideo/best',
-    'outtmpl': './Downloads/temp-%(id)s.%(ext)s',
-    'logger': MyLogger(),
-    'progress_hooks': [my_hook],
+    'outtmpl': f'{TEMP_PATH}/%(id)s.%(ext)s',
+    'logger': DlLogger(),
+    'progress_hooks': [dl_hook],
 }
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(vid_urls)
 
-for vid_id in vid_ids:
-    # print("getting first frame")
-    vidcap = cv2.VideoCapture(f'{dirpath}/temp-{vid_id}.webm')
-    success,image = vidcap.read()
-    cv2.imwrite("frame.png", image)   
-    success,image = vidcap.read()
-    filename = "frame.png"
-    audiofile = eyed3.load(f'{dirpath}/temp-{vid_id}.mp3')
-    if (audiofile.tag == None):
-        audiofile.initTag()
-    audiofile.tag.images.set(ImageFrame.FRONT_COVER, open(filename,'rb').read(), 'image/png')
-    audiofile.tag.save(version=eyed3.id3.ID3_V2_3)
+class VideoDl():
+    def __init__(self, video_url):
+        self.video_url = video_url
+        self.video_id = VIDEO_ID_REGEX.match(video_url).groups()[0]
+        self.audio_path = f'{TEMP_PATH}/{self.video_id}.mp3'
+        self.video_path = f'{TEMP_PATH}/{self.video_id}.webm'
+        self.image_path = f'{TEMP_PATH}/{self.video_id}.png'
+
+
+
+
+if len(sys.argv) <= 1:
+    print("Please input a youtube URL.")
+    quit()
+
+if os.path.exists(TEMP_PATH):
+    shutil.rmtree(TEMP_PATH)
+os.mkdir(TEMP_PATH)
+
+video_dls = [VideoDl(video_url) for video_url in sys.argv[1:]]
+
+for video_dl in video_dls:
+    print(video_dl.video_url)
+    dl_format = "AUDIO"
+    with youtube_dl.YoutubeDL(AUDIO_YTDL_OPTS) as ydl:
+        ydl.download([video_dl.video_url])
+    dl_format = "VIDEO"
+    with youtube_dl.YoutubeDL(VIDEO_YTDL_OPTS) as ydl:
+        ydl.download([video_dl.video_url])
+
+    success, image = cv2.VideoCapture(video_dl.video_path).read()
+    if success:
+        cv2.imwrite(video_dl.image_path, image)
+        audiofile = eyed3.load(video_dl.audio_path)
+        if not audiofile.tag:
+            audiofile.initTag()
+        audiofile.tag.images.set(ImageFrame.FRONT_COVER, open(video_dl.image_path,'rb').read(), 'image/png')
+        audiofile.tag.save(version=eyed3.id3.ID3_V2_3)
 
 # reg = re.compile(r"https:\/\/www\.youtube\.com\/watch\?v=([A-Za-z0-9]+).*")
 
@@ -116,7 +124,7 @@ for vid_id in vid_ids:
 #         img.save(filename)
 
     # id = meta['id']
-    # audiofile = eyed3.load(f'{dirpath}/temp-{vid_id}.mp3')
+    # audiofile = eyed3.load(f'{DIR_PATH}/temp-{vid_id}.mp3')
     # if (audiofile.tag == None):
     #     audiofile.initTag()
 
